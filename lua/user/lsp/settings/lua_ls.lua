6cc94ae6fa20d82c95faf199ed13b10c252165ab
Enigma <60807619+V-For-Vivek@users.noreply.github.com>
2023-03-24 23:09:30 +0530

    settings changes

--- lua/user/lsp/settings/lua_ls.lua
+++ lua/user/lsp/settings/lua_ls.lua
@@ -1,16 +1,17 @@
 return {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim" },
-            },
-            workspace = {
-                library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.stdpath("config") .. "/lua"] = true,
-                    "${3rd}/luv/library",
-                },
-            },
-        },
-    },
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+					[vim.fn.expand("${3rd}/luv/library")] = true,
+				},
+				checkThirdParty = false
+			},
+		},
+	},
 }
